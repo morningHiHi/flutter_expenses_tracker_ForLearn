8f6c67d422e6bc6a691897fc387f7857d30468db
Nguyen Thac Sang <sangnguyenthac@gmail.com>
2025-07-22 11:06:29 +0700

    create _openAddExpenseOverlay method

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -25,12 +25,25 @@ class _ExpensesState extends State<Expenses> {
       category: Category.leisure,
     ),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => Text('Modal bottom sheet'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter ExpenseTracker'),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: Column(
         children: [
