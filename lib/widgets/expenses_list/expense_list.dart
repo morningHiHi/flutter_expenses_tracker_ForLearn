0b5a70236f5ec8170858b0b567da9f51a08c3914
Nguyen Thac Sang <sangnguyenthac@gmail.com>
2025-08-24 23:05:57 +0700

    adding Dismissible widget to swipe left to right

--- lib/widgets/expenses_list/expense_list.dart
+++ lib/widgets/expenses_list/expense_list.dart
@@ -3,16 +3,23 @@ import 'package:expenses_tracker/widgets/expenses_list/expenses_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseList extends StatelessWidget {
-  const ExpenseList({super.key, required this.expenses});
+  const ExpenseList({super.key, required this.expenses,required this.onRemoveExpense});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
       //itemBuilder: (ctx, index) => Text(expenses[index].title),
-      itemBuilder: (ctx, index) => ExpensesItem(expense: expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpensesItem(expense: expenses[index]),
+      ),
     );
   }
 }
