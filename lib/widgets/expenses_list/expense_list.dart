0192af103d025618b2eea6f97493d8aa65ea324b
Nguyen Thac Sang <sangnguyenthac@gmail.com>
2025-08-26 10:34:14 +0700

    make a backgound color for dismissible

--- lib/widgets/expenses_list/expense_list.dart
+++ lib/widgets/expenses_list/expense_list.dart
@@ -3,7 +3,11 @@ import 'package:expenses_tracker/widgets/expenses_list/expenses_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseList extends StatelessWidget {
-  const ExpenseList({super.key, required this.expenses,required this.onRemoveExpense});
+  const ExpenseList({
+    super.key,
+    required this.expenses,
+    required this.onRemoveExpense,
+  });
 
   final List<Expense> expenses;
   final void Function(Expense expense) onRemoveExpense;
@@ -15,6 +19,12 @@ class ExpenseList extends StatelessWidget {
       //itemBuilder: (ctx, index) => Text(expenses[index].title),
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withValues(alpha: 0.75),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
