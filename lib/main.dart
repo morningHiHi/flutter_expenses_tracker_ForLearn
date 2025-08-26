d4c20b27f1d5364270998ee7bea77a8660ceb648
Nguyen Thac Sang <sangnguyenthac@gmail.com>
2025-08-26 10:32:50 +0700

    make a dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,35 @@ import 'package:expenses_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 ColorScheme kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 123, 110, 240),
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+ColorScheme kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
 );
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark().copyWith(
+        //useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardThemeData().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.primaryContainer
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
-        useMaterial3: true,
-        scaffoldBackgroundColor: Colors.amberAccent,
+        //useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 157, 200, 207),
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
@@ -34,6 +53,7 @@ void main() {
           ),
         ),
       ),
+      //themeMode: ThemeMode.system,
       home: Expenses(),
     ),
   );
